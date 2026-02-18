d553fa7f348e4600d21a9c21bed139ad4a8ced0e
marcosdgr <marcos.ciruja22@gmail.com>
2026-02-18 19:10:33 -0300

    creo repo y conexion de mongo

---
+++ index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+
+
+dotenv.config();
+
+connectDB();
+
+app.listen(process.env.PORT, ()=>{
+   console.log("Servidor corriendo");
+});
